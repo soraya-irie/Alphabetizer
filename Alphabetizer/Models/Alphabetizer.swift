08dfe6461875e9a109ab7b6f6e091a8040644a70
soraya-ire <rujiangkongshi@gmail.com>
2026-04-10 14:34:45 +0900

    Complete Section2 Step3 - complete a game with logic

--- Alphabetizer/Models/Alphabetizer.swift
+++ Alphabetizer/Models/Alphabetizer.swift
@@ -2,7 +2,12 @@ import Foundation
 
 class Alphabetizer {
     private let tileCount = 3
+    private var vocab: Vocabulary
 
     var tiles = [Tile]()
     var score = 0
+
+    init(vocab: Vocabulary = .oceanAnimals) {
+        self.vocab = vocab
+    }
 }
