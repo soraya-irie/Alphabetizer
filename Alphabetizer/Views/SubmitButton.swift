657bab04f38c5c1794ed0904b06d593886180d39
soraya-ire <rujiangkongshi@gmail.com>
2026-04-10 19:15:46 +0900

    Complete Section4 Step2 - complete a game with logic

--- Alphabetizer/Views/SubmitButton.swift
+++ Alphabetizer/Views/SubmitButton.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 
 struct SubmitButton: View {
+    @Environment(Alphabetizer.self) private var alphabetizer
+
     var body: some View {
         Button {
-            // TODO: Submit
+            alphabetizer.submit()
         } label: {
             Image(systemName: "play.circle")
                 .font(.system(size: 60))
@@ -19,4 +21,5 @@ struct SubmitButton: View {
 
 #Preview {
     SubmitButton()
+        .environment(Alphabetizer())
 }
